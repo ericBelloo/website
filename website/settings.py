2ee66c2fb629ae9849f9b35326ed1eb00d5a8a3e
Eric <ipnMagazinee@gmail.com>
2020-02-04 21:22:31 -0600

    add static

--- website/settings.py
+++ website/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Sass files (Scss)
 STATICFILES_FINDERS = [
@@ -139,6 +138,7 @@ SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
 # documents files (docx, pdf)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 
 DOCUMENTS = MEDIA_ROOT + '/documents'
 IMAGES = MEDIA_ROOT + '/images'

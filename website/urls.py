459069513e1daf235b6caf995e6a7435bda20896
Eric <ipnMagazinee@gmail.com>
2020-02-05 15:16:55 -0600

    change urls

--- website/urls.py
+++ website/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     # my urls
     path('', include('apps.home.urls')),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
